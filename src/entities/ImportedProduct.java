2d01f18137da9bbdc6b0c2037fc38039c49f3def
Antonio Pires <tonan@globo.com>
2020-09-23 15:46:17 -0300

    Projeto iniciado

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class ImportedProduct extends Product {
+	
+	private Double customsFee;
+
+	public ImportedProduct() {
+		super();
+	}
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+	
+	public Double totalPrice() {
+		return getPrice()+customsFee;
+	}
+
+	@Override
+	public String priceTag() {
+		return super.priceTag()+" $"+ totalPrice()+" (Customs Fee: $"
+	}
+}
